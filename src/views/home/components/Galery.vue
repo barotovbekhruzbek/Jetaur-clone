463717b9c355b89934754591feb9b8443d5593dc
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-02-14 11:58:52 +0500

    changed all

--- src/views/home/components/Galery.vue
+++ src/views/home/components/Galery.vue
@@ -2,13 +2,13 @@
   <div class="galery-title">
     Galereya
   </div>
-<div class="container">
-  <div class="gallery">
-    <div v-for="(image, index) in images" :key="index" @click="enlargeImage(image.img)">
-      <img :src="image.img" alt="gallery image">
+  <div class="container">
+    <div class="gallery">
+      <div v-for="(image, index) in images" :key="index" @click="enlargeImage(image.img)">
+        <img :src="image.img" alt="gallery image">
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -29,24 +29,18 @@ export default {
         {
           img: require('../../../images/gellery/big/4.jpg')
         },
-
         {
           img: require('../../../images/gellery/big/5.jpg')
         },
-
         {
           img: require('../../../images/gellery/big/6.jpg')
         },
-
         {
           img: require('../../../images/gellery/big/7.jpg')
         },
-
         {
           img: require('../../../images/gellery/big/8.jpg')
         },
-
-
       ],
       isEnlarged: false,
       enlargedImage: null
@@ -85,55 +79,29 @@ export default {
   line-height: 67px;
   color: #212529;
   padding-bottom: 20px;
-
 }
-.gallery {
- display: grid;
-  grid-template-columns: auto auto auto auto;
-
-  @include xl {
-    grid-template-columns: auto auto auto;
+  .gallery {
+    display: grid;
+    grid-template-columns: auto auto auto auto;
   }
-@include lg {
-  grid-template-columns: auto auto;
-}
-  @include sm {
-    grid-template-columns: auto;
-    align-items: center;
-}
-
-.gallery img {
-  height: 200px;
-  width: 307px;
-  @include lg {
-    width: 400px;
-    height: 250px;
-  }
-  @include md {
+  .gallery img {
     width: 307px;
     height: 200px;
-
   }
-  @include sm {
-  width: 300px;
+  .enlarged-image {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 999;
   }
-}
-
-.enlarged-image {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.7);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
-}
-
-.enlarged-image img {
-  max-width: 80%;
-  max-height: 80%;
-}}
+  .enlarged-image img {
+    max-width: 80%;
+    max-height: 80%;
+  }
 </style>
